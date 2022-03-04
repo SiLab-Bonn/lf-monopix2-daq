724270780ed4961dafc9be67e086180c6b9b9517
Ivan Caicedo <idcs0906@gmail.com>
2022-03-04 15:28:33 +0100

    ENH: Add formatters of FE-I4 and MIMOSA26 telescope data for Beam Telescope Analysis. Interpretation is optional, since e.g. the output of the FE-I4 from the event builder should only be formatted but not interpreted. They take into account the extra fields required to make timing cuts in BTA (Which should be present in ALL devices).

---
+++ monopix2_daq/analysis/pybar_fei4_BTAconverter_lfmonopix_format.py
@@ -0,0 +1,191 @@
+"""This script prepares FE-I4 test beam raw data recorded by pyBAR to be analyzed by the simple python test beam analysis.
+An installation of pyBAR is required: https://silab-redmine.physik.uni-bonn.de/projects/pybar
+- This script does for each DUT in parallel
+  - Create a hit tables from the raw data
+  - Align the hit table event number to the trigger number to be able to correlate hits in time
+  - Rename and select hit info needed for further analysis.
+"""
+
+import logging
+import os
+from multiprocessing import Pool
+
+import numpy as np
+import tables as tb
+
+from tqdm import tqdm
+
+from pybar.analysis.analyze_raw_data import AnalyzeRawData
+
+from beam_telescope_analysis.hit_analysis import default_hits_dtype
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+
+beam_telescope_analysis_dtype = np.dtype([
+    ('event_number', np.int64),
+    ('frame', np.uint8),
+    ('column', np.uint16),
+    ('row', np.uint16),
+    ('charge', np.uint16),
+#    ('tdc_value', np.uint16),
+#    ('tdc_timestamp', np.uint16),
+#    ('tdc_status', np.uint8),
+    ('tot', "<f4"),
+    ('phase', "u1"),
+    ('phase_quality', "u1"),
+    ('veto_flg', "u1")])
+
+
+def process_dut(raw_data_file, output_filename=None, trigger_data_format=0, interpret_flag=True, format_flag=True, format_endstring='_interpreted.h5'):
+    ''' Process and format raw data.
+
+    Parameters
+    ----------
+    raw_data_file : string or list of strings
+        Filename(s) of the raw data file(s).
+    output_filename : string
+        Filename of the output interpreted and formatted data file.
+    trigger_data_format : int
+        Trigger/TLU FSM data mode.
+
+    Returns
+    -------
+    output_filename : string
+        Filename of the output interpreted and formatted data file.
+    '''
+    if isinstance(raw_data_file, (list, tuple)):
+        raw_data_filename = os.path.splitext(sorted(raw_data_file)[0])[0]  # get filename with the lowest index
+    else:  # string
+        raw_data_filename = os.path.splitext(raw_data_file)[0]    
+
+    if interpret_flag==True: 
+        analyze_raw_data(input_filename=raw_data_file, trigger_data_format=trigger_data_format)
+    else:
+        pass
+    
+    if format_flag==True:   
+        output_filename = format_hit_table(input_filename=raw_data_filename + format_endstring, output_filename=output_filename)
+        return output_filename
+    else:
+        pass
+
+    return raw_data_filename
+    
+def analyze_raw_data(input_filename, output_filename=None, trigger_data_format=0):  # FE-I4 raw data analysis
+    '''Std. raw data analysis of FE-I4 data. A hit table is created for further analysis.
+
+    Parameters
+    ----------
+    input_filename : string
+        Filename of the input raw data file.
+    output_filename : string
+        Filename of the output interpreted data file.
+    '''
+    with AnalyzeRawData(raw_data_file=input_filename, analyzed_data_file=output_filename, create_pdf=True) as analyze_raw_data:
+        # analyze_raw_data.align_at_trigger_number = True  # if trigger number is at the beginning of each event activate this for event alignment
+        analyze_raw_data.trigger_data_format = trigger_data_format
+        analyze_raw_data.use_tdc_word = False
+        analyze_raw_data.create_hit_table = True
+        analyze_raw_data.create_meta_event_index = True
+        analyze_raw_data.create_trigger_error_hist = True
+        analyze_raw_data.create_rel_bcid_hist = True
+        analyze_raw_data.create_error_hist = True
+        analyze_raw_data.create_service_record_hist = True
+        analyze_raw_data.create_occupancy_hist = True
+        analyze_raw_data.create_tot_hist = True
+        analyze_raw_data.create_source_scan_hist = True
+        analyze_raw_data.create_cluster_size_hist = True
+        analyze_raw_data.create_cluster_tot_hist = True
+        analyze_raw_data.align_at_trigger = True
+        analyze_raw_data.fei4b = False
+        analyze_raw_data.create_empty_event_hits = False
+        # analyze_raw_data.n_bcid = 16
+        # analyze_raw_data.max_tot_value = 13
+        analyze_raw_data.interpreter.set_warning_output(False)
+        analyze_raw_data.interpret_word_table()
+        analyze_raw_data.interpreter.print_summary()
+        analyze_raw_data.plot_histograms()
+
+
+def format_hit_table(input_filename, output_filename=None, chunk_size=1000000):
+    ''' Selects and renames important columns for test beam analysis and stores them into a new file.
+
+    Parameters
+    ----------
+    input_filename : string
+        Filename of the input interpreted data file.
+    output_filename : string
+        Filename of the output interpreted and formatted data file.
+        If None, the filename will be generated.
+    chunk_size : uint
+        Chunk size of the data when reading from file.
+
+    Returns
+    -------
+    output_filename : string
+        Filename of the output interpreted and formatted data file.
+    '''
+    if output_filename is None:
+        output_filename = os.path.splitext(input_filename)[0] + '_formatted_fei4.h5'
+
+    logging.info('Format hit table in %s', input_filename)
+    with tb.open_file(filename=input_filename, mode='r') as in_file_h5:
+        last_event_number = np.zeros(shape=1, dtype=np.int64)
+        input_hits_table = in_file_h5.root.Hits
+        with tb.open_file(filename=output_filename, mode='w') as out_file_h5:
+            output_hits_table = out_file_h5.create_table(
+                where=out_file_h5.root,
+                name='Hits',
+                description=beam_telescope_analysis_dtype,
+                title='Hits for test beam analysis',
+                filters=tb.Filters(
+                    complib='blosc',
+                    complevel=5,
+                    fletcher32=False))
+            for read_index in tqdm(range(0, input_hits_table.nrows, chunk_size)):
+                hits_chunk = input_hits_table.read(read_index, read_index + chunk_size)
+                if np.any(np.diff(np.concatenate((last_event_number, hits_chunk['event_number']))) < 0):
+                    raise RuntimeError('The event number does not increase.')
+                last_event_number = hits_chunk['event_number'][-1:]
+                hits_data_formatted = np.zeros(shape=hits_chunk.shape[0], dtype=beam_telescope_analysis_dtype)
+                hits_data_formatted['event_number'] = hits_chunk['event_number']
+                hits_data_formatted['frame'] = hits_chunk['relative_BCID']
+                hits_data_formatted['column'] = hits_chunk['column']
+                hits_data_formatted['row'] = hits_chunk['row']
+                hits_data_formatted['charge'] = hits_chunk['tot']
+                #hits_data_formatted['tdc_value'] = 0
+                #hits_data_formatted['tdc_timestamp'] = 0
+                #hits_data_formatted['tdc_status'] = 0
+                hits_data_formatted['tot'] = hits_chunk['tot']
+                hits_data_formatted['phase'] = hits_chunk['TDC_time_stamp']
+                hits_data_formatted['phase_quality'] = hits_chunk['TDC_trigger_distance']
+                output_hits_table.append(hits_data_formatted)
+                output_hits_table.flush()
+
+    return output_filename
+
+
+if __name__ == "__main__":
+    # Multi file processing
+    # Input raw data filenames
+    raw_data_files = [ "...ext_trigger_scan.h5"
+                      ]
+    trigger_data_format = 2
+    # Flags to determine if the FE-I4 will be intepreted and/or formatted.
+    # If any filter has been applied to interpreted FE data (e.g. during event building) the file only has to be formatted.
+    interpret_flag=False
+    format_flag=True
+    # Endstring of a possibily modified interpreted file.
+    format_endstring='_interpreted_valid.h5'
+    # Simultaneous the processing of the files. The output is a formatted hit table.
+    pool = Pool()
+    results = [pool.apply_async(process_dut, kwds={'raw_data_file': raw_data_file, 'trigger_data_format': trigger_data_format, 'interpret_flag': interpret_flag
+                                                   , 'format_flag': format_flag, 'format_endstring': format_endstring}) for raw_data_file in raw_data_files]
+    [result.get()[0] for result in results]
+    pool.close()
+    pool.join()
+    # Single file processing
+    # Input raw data filename
+    # raw_data_file = 'pybar_raw_data.h5'
+    # process_dut(raw_data_file=raw_data_file, trigger_data_format=trigger_data_format)
---
+++ monopix2_daq/analysis/pymosa_BTAconverter_lfmonopix_format.py
@@ -0,0 +1,165 @@
+'''Example how to use the M26 data interpreter. A hit table is created from raw data, additionally events are build using the TLU data words.
+    At the end, the hit table is formatted into the correct data format needed for testbeam analysis.
+'''
+
+import logging
+import os
+
+import numpy as np
+import tables as tb
+from multiprocessing import Pool
+
+from tqdm import tqdm
+
+from contextlib2 import ExitStack
+
+from pymosa_mimosa26_interpreter import data_interpreter
+from pymosa_mimosa26_interpreter import raw_data_interpreter
+
+from beam_telescope_analysis.hit_analysis import default_hits_dtype
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+
+beam_telescope_analysis_dtype = np.dtype([
+    ('event_number', np.int64),
+    ('frame', np.uint8),
+    ('column', np.uint16),
+    ('row', np.uint16),
+    ('charge', np.uint16),
+#    ('tdc_value', np.uint16),
+#    ('tdc_timestamp', np.uint16),
+#    ('tdc_status', np.uint8),
+    ('tot', "<f4"),
+    ('phase', "u1"),
+    ('phase_quality', "u1"),
+    ('veto_flg', "u1")])
+
+
+def process_dut(raw_data_file, output_filename=None, output_filenames=None, analyze_m26_header_ids=None, trigger_data_format=2, timing_offset=None):
+    ''' Process and format raw data.
+
+    Parameters
+    ----------
+    raw_data_file : string
+        Filename of the raw data file.
+    output_filename : string
+        Filename of the interpreted data file.
+    output_filenames : list of strings
+        Filenames of the interpreted data files for each plane.
+        If None, the filenames will be generated.
+    trigger_data_format : int
+        Trigger/TLU FSM data mode.
+    analyze_m26_header_ids : list
+        List of Mimosa26 header IDs that will be interpreted.
+        If None, the value defaults to the global value pyBAR_mimosa26_interpreter.raw_data_interpreter.DEFAULT_PYMOSA_M26_HEADER_IDS.
+    timing_offset : int
+        Timing offset for the pyBAR_mimosa26_interpreter. The value has impact on the start of frame to trigger word alignment.
+
+    Returns
+    -------
+    output_filenames : list of strings
+        Filenames of the output interpreted and formatted data files.
+    '''
+    with data_interpreter.DataInterpreter(raw_data_file=raw_data_file, analyzed_data_file=output_filename, analyze_m26_header_ids=analyze_m26_header_ids, trigger_data_format=trigger_data_format, timing_offset=timing_offset, create_pdf=True) as mimosa_data_interpreter:
+        mimosa_data_interpreter.create_occupancy_hist = True
+        mimosa_data_interpreter.create_error_hist = True
+        mimosa_data_interpreter.create_hit_table = True
+        mimosa_data_interpreter.interpret_word_table()  # interpret raw data
+        output_filename = mimosa_data_interpreter.analyzed_data_file
+        analyze_m26_header_ids = mimosa_data_interpreter.analyze_m26_header_ids
+    output_filenames = format_hit_table(input_filename=output_filename, output_filenames=output_filenames, analyze_m26_header_ids=analyze_m26_header_ids)
+    return output_filenames
+
+
+def format_hit_table(input_filename, output_filenames=None, analyze_m26_header_ids=None, chunk_size=1000000):
+    ''' Selects and renames important columns for test beam analysis and stores them into a new file.
+
+    Parameters
+    ----------
+    input_filename : string
+        Filename of the input interpreted data file.
+    output_filenames : list of strings
+        Filenames of the interpreted data files for each plane.
+        If None, the filenames will be generated.
+    analyze_m26_header_ids : list
+        List of Mimosa26 header IDs that will be interpreted.
+        If None, the value defaults to the global value pyBAR_mimosa26_interpreter.raw_data_interpreter.DEFAULT_PYMOSA_M26_HEADER_IDS.
+    chunk_size : uint
+        Chunk size of the data when reading from file.
+
+    Returns
+    -------
+    output_filenames : list
+        Filenames of the interpreted data files for each plane.
+    '''
+
+    logging.info('Formatting hit table...')
+
+    if analyze_m26_header_ids is None:
+        analyze_m26_header_ids = raw_data_interpreter.DEFAULT_PYMOSA_M26_HEADER_IDS
+    else:
+        analyze_m26_header_ids = analyze_m26_header_ids
+    if output_filenames:
+        if len(output_filenames) != len(analyze_m26_header_ids):
+            raise ValueError('Output filenames must be a list of length %d.' % len(analyze_m26_header_ids))
+    else:
+        output_filenames = [(os.path.splitext(input_filename)[0] + '_header_id_' + str(plane_header_id) + '.h5') for plane_header_id in analyze_m26_header_ids]
+    with tb.open_file(filename=input_filename, mode='r') as in_file_h5:
+        last_event_number = np.zeros(shape=1, dtype=np.int64)
+        input_hits_table = in_file_h5.root.Hits
+        with ExitStack() as output_files_stack:
+            out_files_h5 = [output_files_stack.enter_context(tb.open_file(filename=filename, mode='w')) for filename in output_filenames]
+            output_hits_tables = []
+            for out_file_h5 in out_files_h5:
+                output_hits_table = out_file_h5.create_table(
+                    where=out_file_h5.root,
+                    name='Hits',
+                    description=beam_telescope_analysis_dtype,
+                    title='Hits for test beam analysis',
+                    filters=tb.Filters(
+                        complib='blosc',
+                        complevel=5,
+                        fletcher32=False))
+                output_hits_tables.append(output_hits_table)
+
+            pbar = tqdm(total=input_hits_table.nrows, ncols=80)
+            for read_index in range(0, input_hits_table.nrows, chunk_size):
+                hits_chunk = input_hits_table.read(read_index, read_index + chunk_size)
+                if np.any(np.diff(np.concatenate((last_event_number, hits_chunk['event_number']))) < 0):
+                    raise RuntimeError('The event number does not increase.')
+                last_event_number = hits_chunk['event_number'][-1:]
+                for plane_index, output_hits_table in enumerate(output_hits_tables):
+                    plane_header_id = analyze_m26_header_ids[plane_index]
+                    selected_hits = (hits_chunk['plane'] == plane_header_id)
+                    hits_data_formatted = np.zeros(shape=np.count_nonzero(selected_hits), dtype=beam_telescope_analysis_dtype)
+                    # Format data for testbeam analysis
+                    hits_data_formatted['event_number'] = hits_chunk[selected_hits]['event_number']
+                    hits_data_formatted['frame'] = 0
+                    hits_data_formatted['column'] = hits_chunk[selected_hits]['column'] + 1
+                    hits_data_formatted['row'] = hits_chunk[selected_hits]['row'] + 1
+                    hits_data_formatted['charge'] = 0
+                    # hits_data_formatted['tdc_value'] = 0
+                    # hits_data_formatted['tdc_timestamp'] = 0
+                    # hits_data_formatted['tdc_status'] = 0
+                    # Append data to table
+                    output_hits_table.append(hits_data_formatted)
+                    output_hits_table.flush()
+                pbar.update(hits_chunk.shape[0])
+            pbar.close()
+
+    return output_filenames
+
+
+if __name__ == "__main__":
+    # Single file processing
+    # Input raw data filename
+    raw_data_files = [  
+                        '..._M26_TELESCOPE.h5'
+                      ]
+    #process_dut(raw_data_file=raw_data_file, trigger_data_format=2, analyze_m26_header_ids=[1,2,3,4,6])
+    pool = Pool()
+    results = [pool.apply_async(process_dut, kwds={'raw_data_file': raw_data_file, 'trigger_data_format': 2, 'analyze_m26_header_ids':[1,2,3,4,6]}) for raw_data_file in raw_data_files]
+    [result.get()[0] for result in results]
+    pool.close()
+    pool.join()
